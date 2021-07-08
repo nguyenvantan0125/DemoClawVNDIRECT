686501509a36ce1df35b88be96063cf697e45142
nguyenvantan0125 <nguyenvantan0125@gmail.com>
2021-07-08 23:24:20 +0700

    Update DemoClawVNDIRECT.py
    2324

--- DemoClawVNDIRECT/DemoClawVNDIRECT.py
+++ DemoClawVNDIRECT/DemoClawVNDIRECT.py
@@ -1,16 +1,16 @@
 from selenium import  webdriver
 
 driver = webdriver.Chrome(executable_path =r'D:\VNDIRECT\DemoClawVNDIRECT\chromedriver.exe' )
-driver.get("https://trade.vndirect.com.vn/chung-khoan/danh-muc")
+driver.get("https://trade.vndirect.com.vn/chung-khoan/vn30")
 
 eleUser = driver.find_element_by_css_selector("input[name='username']")
 elePassword =  driver.find_element_by_css_selector("input[name='password']")
-eleSubmit= driver.find_element_by_xpath("//span[text()='Đăng nhập']")
+eleSubmit= driver.find_element_by_xpath("//span[text()='Đăng nhập']/parent::*")
 
 def Login(user, password):
     eleUser.send_keys(user)
     elePassword.send_keys(password)
-    eleSubmit.click
+    eleSubmit.submit()
 
 Login("nguyenvantan0125","Vantan@252525")
 print(driver.title)
