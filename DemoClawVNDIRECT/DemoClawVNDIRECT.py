6473bb384fddb708c4c085dec2181bbc0097bece
nguyenvantan0125 <nguyenvantan0125@gmail.com>
2021-07-08 22:00:15 +0700

    update

--- DemoClawVNDIRECT/DemoClawVNDIRECT.py
+++ DemoClawVNDIRECT/DemoClawVNDIRECT.py
@@ -1,1 +1,11 @@
-print("Hello123")
+from selenium import  webdriver
+import time
+
+driver = webdriver.Chrome(executable_path =r'D:\VNDIRECT\DemoClawVNDIRECT\chromedriver.exe' )
+driver.get("https://www.google.com/")
+
+print(driver.title)
+el = driver.find_element_by_name("q")
+el.send_keys("Hinh hoc")
+el.submit()
+driver.quit()
