c631cfb46f46e94385d23533b7fef0c1f962eb50
nguyenvantan0125 <nguyenvantan0125@gmail.com>
2021-07-11 23:20:49 +0700

    2320

--- DemoClawVNDIRECT/Browser.py
+++ DemoClawVNDIRECT/Browser.py
@@ -1,9 +1,6 @@
 from selenium import  webdriver
 from selenium.webdriver.common.by import By
 
-
-
-
 class Browser:
     def __init__(self, pathdriver): # khoi tao browser vs chrome driver
         self.driver = webdriver.Chrome(executable_path=pathdriver)
@@ -11,20 +8,17 @@ class Browser:
     def Loadpage(self, url): # Load page
         self.driver.get(url)
 
-    def Get(self,element):
-        if(element.startswith("//")):
-            return self.driver.find_element(By.XPATH,element)
-        else:
-            return self.driver.find_element(By.CSS_SELECTOR,element)
-
     def tearDown(self):
         self.driver.quit()
 
 
 class Locator:
-    eleUser = "input[name='username']"
-    elePassword =  "input[name='password']"
-    eleSubmit= "//span[text()='Đăng nhập']/parent::*"
+    def eleUser(self):
+        return self.Get("input[name='username']")
+    def elePassword(self):
+        return self.Get("input[name='password']")
+    def eleSubmit(self):
+        return self.Get("//span[text()='Đăng nhập']/parent::*")
 
     def Get(self,element):
         if(element.startswith("//")):
@@ -32,13 +26,12 @@ class Locator:
         else:
             return self.driver.find_element(By.CSS_SELECTOR,element) 
 
-class VndirectPage(Browser,Locator):    
+class VndirectPage(Browser,Locator):
 
     def __init__(self, pathdriver):
-        super(Browser).__init__(pathdriver)
-
+        super().__init__(self,pathdriver)
 
     def Login(self,user, password):
-        Locator.Get(Locator.eleUser).send_keys(user)
-        Locator.Get(Locator.eleUser).send_keys(password)
+        Locator.eleUser(self).send_keys(user)
+        Locator.elePassword(self).send_keys(password)
        
