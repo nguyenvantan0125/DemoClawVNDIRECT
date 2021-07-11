a95c3b83e68257dea10d7989267ed52ee2a3cc75
nguyenvantan0125 <nguyenvantan0125@gmail.com>
2021-07-11 23:21:00 +0700

    230

--- DemoClawVNDIRECT/DemoClawVNDIRECT.py
+++ DemoClawVNDIRECT/DemoClawVNDIRECT.py
@@ -1,9 +1,9 @@
-from VndirectPage import *
+from Browser import *
 
 Vnpage = VndirectPage(r'D:\VNDIRECT\DemoClawVNDIRECT\chromedriver.exe')
 Vnpage.Loadpage("https://trade.vndirect.com.vn/chung-khoan/vn30")
 
 Vnpage.Login("nguyenvantan0125","Vantan@252525")
 
-Vnpage.tearDown()
+# Vnpage.tearDown()
 
