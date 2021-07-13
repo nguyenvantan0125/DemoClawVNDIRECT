f3519d85f6d6bdf7622ae3a91fadd319f51d37f0
nguyenvantan0125 <nguyenvantan0125@gmail.com>
2021-07-13 22:37:32 +0700

    2237

--- DemoClawVNDIRECT/Browser.py
+++ DemoClawVNDIRECT/Browser.py
@@ -1,15 +1,12 @@
-import time
+from typing import SupportsIndex
 from selenium import  webdriver
-from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
 class Browser:
     def __init__(self, pathdriver): # khoi tao browser vs chrome driver
-        self.driver = webdriver.Chrome(executable_path=pathdriver)        
+        self.driver = webdriver.Chrome(executable_path=pathdriver)
 
     def Loadpage(self, url): # Load page
         self.driver.get(url)
@@ -25,18 +22,24 @@ class Browser:
            
 
 class CafeF(Browser):
-    def __init__(self, pathdriver):
+    def __init__(self, pathdriver):        
         super().__init__(pathdriver)
+
     def eleSearchBox(self):
         return super().Get("#CafeF_SearchKeyword_Company")
     def eleSubmit(self):
         return super().Get(".s-submit")
+    def eleBasicEPS(self):
+        return super().Get("//a[contains(text(),'EPS cơ bản')]/following::div[1]")
+    def eleDilutionEPS(self):
+        return super().Get("//a[contains(text(),'EPS pha loãng')]/following::div[1]")
+    def elePE(self):
+        return super().Get("//b[contains(text(),'P/E')]/following::div[1]")
 
-    def SearchByCode(self,Code):        
+    def SearchByCode(self):
+        Code = str(input("Stock price: "))        
         self.eleSearchBox().send_keys(Code)
-        self.eleSubmit().click()
-        
-        
+        self.eleSubmit().click()       
 
 
 class VndirectPage(Browser):    
@@ -50,6 +53,17 @@ class VndirectPage(Browser):
         return super().Get("input[name='password']")
     def eleSubmit(self):
         return super().Get("//span[text()='Đăng nhập']/parent::*")
+    def eleROAs(self):
+        return super().Get("//label[contains(text(),'ROA')]/parent::*/following-sibling::*[not(@class='chart')]") 
+        # get list element and get multiple text
+    def eleROEs(self):
+        return super().Get("//label[contains(text(),'ROE')]/parent::*/following-sibling::*[not(@class='chart')]")
+    def eleROSs(self):
+        return super().Get("//label[contains(text(),'ROS')]/parent::*/following-sibling::*[not(@class='chart')]")
+    def eleGOSs(self):
+        return super().Get("//label[contains(text(),'GOS')]/parent::*/following-sibling::*[not(@class='chart')]")
+    def eleDARs(self):
+        return super().Get("//label[contains(text(),'DAR')]/parent::*/following-sibling::*[not(@class='chart')]")
 
     def Login(self,user, password):
         self.eleUser().send_keys(user)
--- DemoClawVNDIRECT/DemoClawVNDIRECT.py
+++ DemoClawVNDIRECT/DemoClawVNDIRECT.py
@@ -4,7 +4,7 @@ Vnpage = CafeF(r'D:\VNDIRECT\DemoClawVNDIRECT\chromedriver.exe')
 Vnpage.Loadpage("https://cafef.vn/")
 #Vnpage.Loadpage("https://trade.vndirect.com.vn/chung-khoan/vn30")
 
-Vnpage.SearchByCode("fpt")
+Vnpage.SearchByCode()
 #Vnpage.Login("nguyenvantan","Vantan@252525")
 
 # Vnpage.tearDown()
